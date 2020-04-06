2aff35d762d0c4826d9e6eebec951be5049a68af
Max Pixel <git@m-pixel.com>
2020-04-05 23:28:11 -0700

    Change that was meant to be included in the previous commit.

--- MigraDoc Fluent/Document.cs
+++ MigraDoc Fluent/Document.cs
@@ -1,9 +1,33 @@
 ﻿using System;
+using MigraDoc.DocumentObjectModel;
 
 namespace MigraDoc
 {
 	public static partial class Fluent
 	{
+		public static FluentDocument DeclareDocument() => new Document().AsFluent();
+		public static FluentDocument AsFluent(this Document document) => new FluentDocument(document);
 
+		public static Document AppendSection(this Document document, Action<Section> builder)
+		{
+			builder(document.AddSection());
+			return document;
+		}
+	}
+
+	public struct FluentDocument
+	{
+		public Document Subject { get; }
+
+		public FluentDocument(Document subject)
+		{
+			Subject = subject;
+		}
+
+		public FluentDocument Section(Action<FluentSection> f)
+		{
+			f(new FluentSection(Subject.AddSection()));
+			return this;
+		}
 	}
 }
